21326fd783105dd3d646737d753ece6002991c1e
md_fazal <fazalmohammad980@gmail.com>
2023-06-28 22:03:26 +0530

    added schemas to sanity

--- shareme_backend/schemas/index.js
+++ shareme_backend/schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from "./user"
+import comment from "./comment"
+import pin from "./pin"
+import postedBy from "./postedBy"
+import save from "./save"
+
+export const schemaTypes = [user, comment, pin, postedBy, save]
